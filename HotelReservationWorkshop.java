9e07474a562de70548508d3d4b9af8c7fdb4628a
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-24 22:35:11 +0530

    Welcome to Hotel Reservation Program

---
+++ HotelReservationWorkshop.java
@@ -0,0 +1,19 @@
+package workshop;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class HotelReservationWorkshop 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Hotel Reservation Program");
+		
+	}
+
+}
+
+
+
